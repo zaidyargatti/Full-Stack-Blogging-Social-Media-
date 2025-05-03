eb80b396ab3d83cf5c5b143940da04e067376e55
zaidyargatti <9928zaid@gamil.com>
2025-05-03 16:39:36 +0530

    fixing it ho ja bhai

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,7 +1,7 @@
 // src/context/SocketContext.jsx
 import { createContext, useContext, useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
-import { useAuth } from './Authcontext';
+import { useAuth } from './AuthContext';
 
 const SocketContext = createContext();
 
